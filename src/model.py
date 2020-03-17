f6a12e94e34437d494587d87571efbe121b7f635
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-17 20:37:28 -0300

    update req

--- src/model.py
+++ src/model.py
@@ -1,17 +1,3 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-df = pd.read_csv("full_data.csv")
-df.fillna(0, inplace=True)  
-
-locations = ["Brazil", "Italy", "United States", "United Kingdom", "Iran", "South Korea", "Japan"]
-#tempt = [28]
-
-#for location in locations:
-location = "Japan"
-plt.plot(df["date"][df["location"] == location], df["new_cases"][df["location"] == location], '-', label=location)
-
-plt.grid(axis='x', color='0.95')
-plt.legend(title='Parameter where:')
-plt.title('plt.step(where=...)')
-plt.show()
+import lightgbm as lgb
