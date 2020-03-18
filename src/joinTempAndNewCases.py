0f91236835bc20a1b01775dc8fa4ac99acdd8af8
Rafael Machado <rafafsmachado@gmail.com>
2020-03-18 00:02:51 -0300

    calculated area and geo data frame for new cases dataframe

--- src/joinTempAndNewCases.py
+++ src/joinTempAndNewCases.py
@@ -29,40 +29,78 @@ path = "../data/localData/"
 nc = pd.read_csv(path+"newCases.csv") # to gen run convertDataTo...
 tempt = pd.read_csv(path+"temperature.csv") # to gen run joinTempCsv...
 
-#gnc = geopandas.GeoDataFrame(
-#    nc, geometry=geopandas.points_from_xy(nc.Longitude, nc.Latitude))
-print(nc.head())
+# ---------- CALCULATING RADIUS FOR NEW CASES ----------
+
 countries = nc["Unnamed: 0"]
 latlong = pd.DataFrame()
 latlong["lat"] = nc["lat"]
 latlong["long"] = nc["long"]
 nc = nc.drop(["lat", "long"], axis=1)
-nc["total"] = nc.sum(axis=1)
-print(nc[nc["Unnamed: 0"] == "Cambodia"])
+nc["Total"] = nc.sum(axis=1).astype(int)
 
 cases = 0
 radius = []
-for c in countries:
-    if (cases > 0):
-        if (cases <= 5):
-            radius.append(4.0)
-        elif (cases <= 10):
-            radius.append(6.333333333333334)
-        elif (cases <= 25):
-            radius.append(8.666666666666668)
-        elif (cases <= 100):
-            radius.append(11.0)
-        elif (cases <= 200):
-            radius.append(13.333333333333336)
-        elif (cases <= 500):
-            radius.append(15.666666666666668)
-        elif (cases <= 1000):
-            radius.append(18.0)
-        elif (cases <= 2000):
-            radius.append(20.333333333333336)
-        elif (cases <= 5000):
-            radius.append(22.66666666666667)
-        elif (cases <= 70000):
-            radius.append(25.0)
-        else: # China
-            radius.append(60.0)
+for c in range(0, len(nc)):
+    cases = nc["Total"].iloc[c]
+    if (cases == 0):
+        radius.append(0.0)
+    elif (cases <= 5):
+        radius.append(4.0)
+    elif (cases <= 10):
+        radius.append(6.333333333333334)
+    elif (cases <= 25):
+        radius.append(8.666666666666668)
+    elif (cases <= 100):
+        radius.append(11.0)
+    elif (cases <= 200):
+        radius.append(13.333333333333336)
+    elif (cases <= 500):
+        radius.append(15.666666666666668)
+    elif (cases <= 1000):
+        radius.append(18.0)
+    elif (cases <= 2000):
+        radius.append(20.333333333333336)
+    elif (cases <= 5000):
+        radius.append(22.66666666666667)
+    elif (cases <= 70000):
+        radius.append(25.0)
+    else: # China
+        radius.append(60.0)
+
+nc["Radius"] = radius
+nc["Latitude"] = latlong["lat"]
+nc["Longitude"] = latlong["long"]
+
+#print(nc.head())
+
+# ---------- CREATING GEO DATA FRAME ----------
+
+gnc = geopandas.GeoDataFrame(
+    nc, geometry=geopandas.points_from_xy(nc.Longitude, nc.Latitude))
+
+world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+
+# Restrict to a continent example
+#ax = world[world.continent == 'South America'].plot(
+#    color='white', edgecolor='black')
+
+ax = world.plot(color='white', edgecolor='black')
+
+# We can now plot our ``GeoDataFrame``.
+gnc.plot(ax=ax, color='red')
+
+#plt.show()
+
+# ---------- CALCULATING AREA ----------
+
+area = []
+
+for c in range(0, len(nc)):
+    # Note: maybe multiply the radius by 1000 or a larger number
+    area.append(geodesic_point_buffer(gnc["Latitude"].iloc[c], gnc["Longitude"].iloc[c], gnc["Radius"].iloc[c]))
+
+gnc["Area"] = area
+
+print(gnc.head())
+
+# ---------- TEMPERATURE ----------
