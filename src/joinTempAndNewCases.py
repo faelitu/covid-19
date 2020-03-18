7783fa50943d4bc943463bb61883589a32fd00ff
Rafael Machado <rafafsmachado@gmail.com>
2020-03-17 22:55:22 -0300

    some simple alterations and analysis

--- src/joinTempAndNewCases.py
+++ src/joinTempAndNewCases.py
@@ -31,35 +31,38 @@ tempt = pd.read_csv(path+"temperature.csv") # to gen run joinTempCsv...
 
 #gnc = geopandas.GeoDataFrame(
 #    nc, geometry=geopandas.points_from_xy(nc.Longitude, nc.Latitude))
-print(nc.tail())
-print(nc.columns)
-countries = nc.columns
-del countries["Unnamed: 0"]
-del countries["date"]
+print(nc.head())
+countries = nc["Unnamed: 0"]
+latlong = pd.DataFrame()
+latlong["lat"] = nc["lat"]
+latlong["long"] = nc["long"]
+nc = nc.drop(["lat", "long"], axis=1)
+nc["total"] = nc.sum(axis=1)
+print(nc[nc["Unnamed: 0"] == "Cambodia"])
 
 cases = 0
 radius = []
-
-if (cases > 0):
-    if (cases <= 5):
-        radius.append(4.0)
-    elif (cases <= 10):
-        radius.append(6.333333333333334)
-    elif (cases <= 25):
-        radius.append(8.666666666666668)
-    elif (cases <= 100):
-        radius.append(11.0)
-    elif (cases <= 200):
-        radius.append(13.333333333333336)
-    elif (cases <= 500):
-        radius.append(15.666666666666668)
-    elif (cases <= 1000):
-        radius.append(18.0)
-    elif (cases <= 2000):
-        radius.append(20.333333333333336)
-    elif (cases <= 5000):
-        radius.append(22.66666666666667)
-    elif (cases <= 70000):
-        radius.append(25.0)
-    else: # China
-        radius.append(60.0)
+for c in countries:
+    if (cases > 0):
+        if (cases <= 5):
+            radius.append(4.0)
+        elif (cases <= 10):
+            radius.append(6.333333333333334)
+        elif (cases <= 25):
+            radius.append(8.666666666666668)
+        elif (cases <= 100):
+            radius.append(11.0)
+        elif (cases <= 200):
+            radius.append(13.333333333333336)
+        elif (cases <= 500):
+            radius.append(15.666666666666668)
+        elif (cases <= 1000):
+            radius.append(18.0)
+        elif (cases <= 2000):
+            radius.append(20.333333333333336)
+        elif (cases <= 5000):
+            radius.append(22.66666666666667)
+        elif (cases <= 70000):
+            radius.append(25.0)
+        else: # China
+            radius.append(60.0)
