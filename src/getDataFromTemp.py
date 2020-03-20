dee176f880de7f2fa025df73b51b1908c378d7ae
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2020-03-20 15:09:14 -0300

    init fill gaps

--- src/getDataFromTemp.py
+++ src/getDataFromTemp.py
@@ -1,4 +1,4 @@
-__author__ = "Bruno Vilela, Rafael Machado"
+__author__ = "Bruno Vilela"
 
 import pandas as pd
 import numpy as np
@@ -31,43 +31,5 @@ nc["d"] = R*nc["c"]
 #for i in range(0,450):
 nc["class"] = round((nc["d"] / classValue) - 450)
 
-# ------------ Rafael's alterations below ----------- #
-
-days = []
-days = nc.columns
-days = days.drop(["lat", "long", "a", "c", "d", "class"])
-
-nc = nc.T
-bkp = nc.drop(days)
-nc = nc.drop(["lat", "long", "a", "c", "d", "class"], axis=0)
-nc = nc.replace(0, np.nan)
-nc = nc.reset_index()
-nc = nc.rename(columns={"index": "Date"})
-countries = nc.columns
-countries = countries.drop("Date")
-
-# Converting the column to DateTime format
-nc["Date"] = pd.to_datetime(days, format='%m/%d/%y')
-nc = nc.set_index('Date')
-
-# Imputing using interpolation
-for c in countries:
-    nc[c] = nc[c].interpolate(method='time')
-
-print(nc)
-print(bkp)
-
-nc["Italy"].plot()
-nc["Brazil"].plot()
-plt.show()
-
-bkp = bkp.T
-nc = nc.T
-
-nc = nc.merge(bkp, left_index=True, right_index=True)
-
-print(nc)
-print(bkp)
-
-nc.to_csv(path+"newCasesWithClass_Interpolated.csv")
-temp.to_csv(path+"temperatureWithClass_Interpolated.csv")
+nc.to_csv(path+"newCasesWithClass.csv")
+temp.to_csv(path+"temperatureWithClass.csv")
---
+++ src/interpolate.py
@@ -0,0 +1,50 @@
+__author__ = "Rafael Machado"
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = "../data/localData/"
+nc = pd.read_csv(path+"newCasesWithClass.csv") # to gen run getData...
+temp = pd.read_csv(path+"temperatureWithClass.csv") # to gen run getData...
+
+# ------------ Rafael's alterations below ----------- #
+
+days = []
+days = nc.columns
+days = days.drop(["lat", "long", "a", "c", "d", "class"])
+
+nc = nc.T
+bkp = nc.drop(days)
+nc = nc.drop(["lat", "long", "a", "c", "d", "class"], axis=0)
+nc = nc.replace(0, np.nan)
+nc = nc.reset_index()
+nc = nc.rename(columns={"index": "Date"})
+countries = nc.columns
+countries = countries.drop("Date")
+
+# Converting the column to DateTime format
+nc["Date"] = pd.to_datetime(days, format='%m/%d/%y')
+nc = nc.set_index('Date')
+
+# Imputing using interpolation
+for c in countries:
+    nc[c] = nc[c].interpolate(method='time')
+
+print(nc)
+print(bkp)
+
+nc["Italy"].plot()
+nc["Brazil"].plot()
+plt.show()
+
+bkp = bkp.T
+nc = nc.T
+
+nc = nc.merge(bkp, left_index=True, right_index=True)
+
+print(nc)
+print(bkp)
+
+nc.to_csv(path+"newCasesWithClass_Interpolated.csv")
+temp.to_csv(path+"temperatureWithClass_Interpolated.csv")
